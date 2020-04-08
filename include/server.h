5fe462256cf71f290fb40e96c71cf150e1041127
Matthias Guzmits <matthias.guzmits@bnet.at>
2020-04-08 19:38:25 +0200

    test event-handling on parent-state

--- include/server.h
+++ include/server.h
@@ -3,24 +3,50 @@
 class raft_server final : public so_5::agent_t {
 public:
     struct change_state : public so_5::signal_t{};
-    raft_server(context_t ctx) : so_5::agent_t{std::move(ctx)} {
+
+    struct RequestVote {
+        std::string name;
+    };
+
+    raft_server(context_t ctx) : so_5::agent_t{std::move(ctx)} {}
+
+    void so_define_agent() override {
+
+        server_state.event(&raft_server::request_handler);
+
         server_state.activate();
 
         candidate.event([this](mhood_t<change_state>) {
             std::cout << "candidate" << std::endl;
-            follower.activate();
-            so_5::send<raft_server::change_state>(*this);
-        });
+            leader.time_limit(std::chrono::milliseconds{500}, candidate);
+        })
+        .event(&raft_server::request_handler);
+
         follower.event([this](mhood_t<change_state>) {
             std::cout << "follower" << std::endl;
-        });
+            leader.activate();
+            so_5::send<raft_server::change_state>(*this);
+            leader.time_limit(std::chrono::milliseconds{500}, candidate);
+        })
+        .event(&raft_server::request_handler);
+
         leader.event([this](mhood_t<change_state>) {
             std::cout << "leader" << std::endl;
+            //timer wird bei neuaufruf zurückgesetzt
+            leader.time_limit(std::chrono::milliseconds{100}, candidate);
+            std::this_thread::sleep_for(std::chrono::milliseconds{200});
+            leader.activate();
+            so_5::send<raft_server::change_state>(*this);
         });
+        candidate.on_enter([] {std::cout << "start of candidate" << std::endl;});
     }
 private:
     state_t server_state{this},
-    candidate{initial_substate_of(server_state)},
-    follower{substate_of(server_state)},
+    candidate{substate_of(server_state)},
+    follower{initial_substate_of(server_state)},
     leader{substate_of(server_state)};
+
+    void request_handler(mhood_t<RequestVote> rv) {
+        std::cout << rv->name << std::endl;
+    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,20 @@
 int main() {
     so_5::launch([](so_5::environment_t& env) {
         so_5::mbox_t m;
+        so_5::mbox_t m2;
+
         env.introduce_coop([&](so_5::coop_t& coop) {
             auto test = coop.make_agent<raft_server>();
             m = test->so_direct_mbox();
+            auto test2 = coop.make_agent<raft_server>();
+            m2 = test2->so_direct_mbox();
         });
 
-        so_5::send<raft_server::change_state>(m);
+        //so_5::send<raft_server::change_state>(m);
+        so_5::send<raft_server::RequestVote>(m, "test1");
+        so_5::send<raft_server::RequestVote>(m2, "test2");
+        so_5::send<raft_server::RequestVote>(m2, "test3");
+        so_5::send<raft_server::RequestVote>(m, "test4");
     });
     return 0;
 }
