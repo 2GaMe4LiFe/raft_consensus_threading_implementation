ac421d47874790105101bf7510de337d278378f5
Matthias Guzmits <matthias.guzmits@bnet.at>
2020-04-08 20:15:59 +0200

    add first transition from follower to candidate

--- include/server.h
+++ include/server.h
@@ -1,4 +1,6 @@
 #include <so_5/all.hpp>
+#include <random>
+#include <vector>
 
 class raft_server final : public so_5::agent_t {
 public:
@@ -8,29 +10,45 @@ public:
         std::string name;
     };
 
-    raft_server(context_t ctx) : so_5::agent_t{std::move(ctx)} {}
+    struct AppendEntry {
+        int term;
+    };
+
+    raft_server(context_t ctx) : so_5::agent_t{std::move(ctx)} {
+        server_state.activate();
+    }
 
     void so_define_agent() override {
+        std::random_device dev;
+        std::mt19937 rng(dev());
+        std::uniform_int_distribution dist(150, 300);
+        m_election_timeout = dist(rng);
+        std::cout << m_election_timeout << std::endl;
 
-        server_state.event(&raft_server::request_handler);
-
-        server_state.activate();
+        //server_state.event(&raft_server::request_handler);
 
         candidate.event([this](mhood_t<change_state>) {
+            std::random_device dev;
+            std::mt19937 rng(dev());
+            std::uniform_int_distribution dist(150, 300);
+            m_election_timeout = dist(rng);
+            std::cout << m_election_timeout << std::endl;
+
             std::cout << "candidate" << std::endl;
-            leader.time_limit(std::chrono::milliseconds{500}, candidate);
+
+
         })
         .event(&raft_server::request_handler);
 
         follower.event([this](mhood_t<change_state>) {
+            follower.time_limit(std::chrono::milliseconds{m_election_timeout}, candidate);
+
             std::cout << "follower" << std::endl;
-            leader.activate();
-            so_5::send<raft_server::change_state>(*this);
-            leader.time_limit(std::chrono::milliseconds{500}, candidate);
         })
         .event(&raft_server::request_handler);
 
         leader.event([this](mhood_t<change_state>) {
+
             std::cout << "leader" << std::endl;
             //timer wird bei neuaufruf zurückgesetzt
             leader.time_limit(std::chrono::milliseconds{100}, candidate);
@@ -38,13 +56,27 @@ public:
             leader.activate();
             so_5::send<raft_server::change_state>(*this);
         });
-        candidate.on_enter([] {std::cout << "start of candidate" << std::endl;});
+        candidate.on_enter([this] {
+            std::cout << "start of candidate" << std::endl;
+            so_5::send<change_state>(*this);
+        });
     }
 private:
+    
+
     state_t server_state{this},
     candidate{substate_of(server_state)},
     follower{initial_substate_of(server_state)},
     leader{substate_of(server_state)};
+
+    int m_term;
+    int m_heartbeat_freq;
+    int m_election_timeout;
+    std::vector<so_5::mbox_t> m_mboxes;
+
+    void heartbeat_handler(mhood_t<AppendEntry> ae) {
+        so_5::send<raft_server::change_state>(*this);
+    }
 
     void request_handler(mhood_t<RequestVote> rv) {
         std::cout << rv->name << std::endl;
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #include <so_5/all.hpp>
+#include <vector>
 #include "server.h"
+
+using namespace std;
 
 int main() {
     so_5::launch([](so_5::environment_t& env) {
         so_5::mbox_t m;
         so_5::mbox_t m2;
-
+        vector<so_5::mbox_t> mboxes;
         env.introduce_coop([&](so_5::coop_t& coop) {
             auto test = coop.make_agent<raft_server>();
-            m = test->so_direct_mbox();
+            mboxes.push_back(test->so_direct_mbox());
             auto test2 = coop.make_agent<raft_server>();
-            m2 = test2->so_direct_mbox();
+            mboxes.push_back(test2->so_direct_mbox());
         });
 
-        //so_5::send<raft_server::change_state>(m);
-        so_5::send<raft_server::RequestVote>(m, "test1");
-        so_5::send<raft_server::RequestVote>(m2, "test2");
-        so_5::send<raft_server::RequestVote>(m2, "test3");
-        so_5::send<raft_server::RequestVote>(m, "test4");
+        for (auto el : mboxes) {
+            so_5::send<raft_server::change_state>(el);
+        }
     });
     return 0;
 }
